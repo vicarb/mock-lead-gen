f1ed89b93b9931d8901cefea8d9dbb08b53b3cc6
vicarb <vcardenasb@protonmail.com>
2023-10-09 22:40:01 -0300

    updated submit inquiry button color

--- components/BusinessInquiry/BusinessInquiry.tsx
+++ components/BusinessInquiry/BusinessInquiry.tsx
@@ -23,7 +23,7 @@ const BusinessInquiry = () => {
           <label htmlFor="inquiry" className="block text-gray-700 text-sm font-bold mb-2">Inquiry:</label>
           <textarea id="inquiry" name="inquiry" rows="4" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
         </div>
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+        <button type="submit" className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
           Submit Inquiry
         </button>
       </form>
